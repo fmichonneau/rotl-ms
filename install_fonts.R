787910057e47ec5cbf0f5bae0af0030bfaba16a9
Francois Michonneau <francois.michonneau@gmail.com>
2016-03-21 18:20:13 -0400

    use https instead of ssh to clone repo

--- install_fonts.R
+++ install_fonts.R
@@ -1,5 +1,5 @@
 install_fonts <- function() {
     dir.create("~/.fonts")
-    system("git clone git@github.com:fmichonneau/latex-fonts.git ~/.fonts")
+    system("git clone https://github.com/fmichonneau/latex-fonts.git ~/.fonts")
     system("fc-cache -f -v")
 }
